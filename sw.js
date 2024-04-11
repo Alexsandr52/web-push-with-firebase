207dec978c8376dcfebde81c6e3a624585d8d98e
Alexsandr52 <ak.polyanskiy@gmail.com>
2024-04-11 15:59:43 +0300

    fix pwa

--- sw.js
+++ sw.js
@@ -1,16 +1,51 @@
-const staticDevCoffee = "dev-coffee-site-v1"
-const assets = [
-  "/",
-  "/index.html",
-]
+// This is the "Offline page" service worker
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets)
-    })
-  )
-})
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.mode === 'navigate') {
+      event.respondWith((async () => {
+        try {
+          const preloadResp = await event.preloadResponse;
+  
+          if (preloadResp) {
+            return preloadResp;
+          }
+  
+          const networkResp = await fetch(event.request);
+          return networkResp;
+        } catch (error) {
+  
+          const cache = await caches.open(CACHE);
+          const cachedResp = await cache.match(offlineFallbackPage);
+          return cachedResp;
+        }
+      })());
+    }
+  });
+
 
 self.addEventListener('push', (event) => {
     const notif = event.stopImmediatePropagation.json().notification;
